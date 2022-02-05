9c17528476a575506f0594c6a299f02fed2c3daf
JoaquinAlzugaray <joaquinalzugaray94@gmail.com>
2022-02-05 04:44:30 +0000

    pruebas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,26 +5,34 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [todolist, setTodolist] = useState("");
+	const [todolist, setTodolist] = useState([]);
+	const [newlist, setNewlist] = useState([]);
 
-	const list = (e) => {
-		e.preventDefault();
-		//alert(`hay que hacer: ${todolist}`);
-	};
+	// const list = (e) => {
+	// 	e.preventDefault();
+	// 	// 	//alert(`hay que hacer: ${todolist}`);
+	// };
+	const listatareas = [];
+
+	// const newtodo = () => {
+	// 	return listatareas.push({ todolist });
+	// };
+	console.log({ newlist });
+	// const lis = listatareas.map((item, index) => {
+	// 	return (
+	// 		<li key={index} className="list-group-item">
+	// 			{item}
+	// 		</li>
+	// 	);
+	// });
 
 	return (
 		<div className="container">
-			<form onSubmit={list}>
-				<div className="mb-3">
-					<input onChange={(e) => setTodolist(e.target.value)} />
-					<button type="submit" className="btn btn-primary">
-						Submit
-					</button>
-				</div>
-			</form>
-			<h4>Cosas que hacer</h4>
-			<ul>
-				<li>{todolist}</li>
+			<ul className="list-group">
+				<input onChange={(e) => setTodolist(e.target.value)}></input>
+				<button onClick={(e) => setNewlist(e.target.value)}>
+					Add Item
+				</button>
 			</ul>
 		</div>
 	);
@@ -34,3 +42,4 @@ export default Home;
 
 //https://www.digitalocean.com/community/tutorials/how-to-build-a-react-to-do-app-with-react-hooks
 //https://www.google.com/search?q=TodoMVC+App+with+React+hooks&rlz=1C1GCEA_enIE897IE897&oq=TodoMVC+App+with+React+hooks&aqs=chrome..69i57j69i60.203j0j7&sourceid=chrome&ie=UTF-8
+// https://reactjs.org/docs/hooks-state.html
